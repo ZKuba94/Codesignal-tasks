05c7c7d2fbde4336dedf82159d5da40b1ccf5e19
Jakub <zkuba94@gmail.com>
2023-03-14 13:49:01 +0100

    added solution to task 22

--- intro[21-30]/tasks3.js
+++ intro[21-30]/tasks3.js
@@ -1,12 +1,17 @@
 function isIPv4Address(inputString) {
     const arr = inputString.split('.')
-    if (arr.length!==4 || !arr.every(el=>(el>=0 && el<=255 && el.length>0))) {
+    if (arr.length !== 4 || !arr.every(el => (el >= 0 && el <= 255 && el.length > 0))) {
         return false
-    } else if (arr.some(el=> el.length>1 && el.startsWith('0'))) {
+    } else if (arr.some(el => el.length > 1 && el.startsWith('0'))) {
         return false
     } else return true
 }
 
 function avoidObstacles(inputArray) {
-
+    let step = 1
+    do {
+        if (inputArray.some(el => el % step === 0)) {
+            step++
+        } else return step
+    } while (true)
 }
