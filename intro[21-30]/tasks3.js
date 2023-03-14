1e7320abc94739fe675843f5262da9118e5bbe7a
Jakub <zkuba94@gmail.com>
2023-03-15 00:09:56 +0100

    task 23 finished

--- intro[21-30]/tasks3.js
+++ intro[21-30]/tasks3.js
@@ -15,3 +15,21 @@ function avoidObstacles(inputArray) {
         } else return step
     } while (true)
 }
+
+const image = [[7,4,0,1],
+    [5,6,2,2],
+    [6,10,7,8],
+    [1,4,2,0]]
+function blurBoxes(image) {
+    let newArr = []
+    for (let i = 1; i < image.length - 1; i++) {
+        for (let j = 1; j < image[i].length - 1; j++) {
+            const part = ([image[i - 1].slice(j - 1, j + 2), image[i].slice(j - 1, j + 2), image[i + 1].slice(j - 1, j + 2)].flat())
+            const newPix = Math.floor(part.reduce((acc, curr) => acc + curr) / part.length)
+            newArr[i-1]?newArr[i-1].push(newPix):newArr[i-1] = [newPix]
+        }
+    }
+    return newArr
+}
+// blurBoxes(image)
+
