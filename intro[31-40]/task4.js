c704039c204619a4ea8ccf90063301bc84007ef9
Jakub <zkuba94@gmail.com>
2023-04-17 23:04:02 +0200

    added task 32

--- intro[31-40]/task4.js
+++ intro[31-40]/task4.js
@@ -7,3 +7,9 @@ function depositProfit(deposit, rate, threshold) {
     }
 }
 // depositProfit(100, 20, 170)
+function absoluteValuesSumMinimization(a) {
+    const s= a.map(b => a.reduce((p,c) => p=p+Math.abs(c-b),0))
+    const x = a.find((el,index) => index === s.findIndex(el2 => el2 === Math.min(...s)))
+    return console.log(x)
+}
+// absoluteValuesSumMinimization([2,4,7])
